317ebb0625bb1bf0c3278720da486350e4e65277
Sasha <lekan1992@gmail.com>
2013-08-25 00:41:55 +0300

    Try?2

--- Progect/src/DiscardServerHandler.java
+++ Progect/src/DiscardServerHandler.java
@@ -1,30 +1,34 @@
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.ReferenceCountUtil;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Handles a server-side channel.
  */
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
+    private static final Logger logger = Logger.getLogger(DiscardServerHandler.class.getName());
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-        ByteBuf in = (ByteBuf) msg;
-        try {
-            while (in.isReadable()) { // (1)
-                System.out.print((char) in.readByte());
-                System.out.flush();
-            }
-        } finally {
-            ReferenceCountUtil.release(msg); // (2)
-        }
+        ctx.write(msg);
+        ByteBuf buf = (ByteBuf)msg;
+        System.out.print(buf.readByte());
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx){
+        ctx.flush();
+        System.out.flush();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
         // Close the connection when an exception is raised.
-        cause.printStackTrace();
+        logger.log(Level.WARNING, "Unexpected exception from downstream.", cause);
         ctx.close();
     }
 }
